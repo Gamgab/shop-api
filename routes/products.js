d9b84dbca48dea90fe706009fe95e3f64738937e
Gamgab <gamgab@hotmail.fr>
2023-03-21 18:14:24 +0100

    ajout de la route delete

--- routes/products.js
+++ routes/products.js
@@ -37,10 +37,54 @@ router.post("/", isAdmin, async (req, res) => {
   }
 });
 
+// GET ALL PRODUCTS
+
 router.get("/", async (req, res) => {
   try {
     const products = await Product.find();
     res.status(200).send(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+// FIND ONE PRODUCT
+
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).send(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+// DELETE ONE PRODUCT
+
+router.delete("/:id", isAdmin, async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) return res.status(404).send("Produit non trouvé ...");
+
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    res.status(200).send(deletedProduct);
+
+    /* SPPRIMER IMAGE DE CLOUDINARY AVEC UN PUBLIC_ID
+    if (product.image.public_id) {
+      const destroyResponse = await cloudinary.uploader.destroy(
+        product.image.public_id
+      );
+      if (destroyResponse) {
+        const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+        res.status(200).send(deletedProduct);
+      }
+    } else {
+      console.log("Action stoppée. Impossible de supprimer l'image...")
+    }
+    */
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
